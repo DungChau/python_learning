ed6a59cc72a761c8514647a195fa126368c14786
Dung Chau <viet.lunar@gmail.com>
2013-02-02 14:50:53 -0800

    add calculate_score to table class

--- dinner_party.py
+++ dinner_party.py
@@ -6,6 +6,8 @@ def main():
 	dinner.write_output(people)
 	for person in people:
 		print(person.preference)
+	table = Table(len(people), people)
+	print(table.calculate_score())
 
 class DinnerParty(object):
 	"""docstring for DinnerParty"""
@@ -16,7 +18,7 @@ class DinnerParty(object):
 	# to the function: self AND filename
 	def parse_file(self, filename):
 		file = open(filename)
-		index = 1
+		index = 0
 		people = []
 		numberOfGuests = int(file.readline())
 		for line in file:
@@ -46,6 +48,9 @@ class Person(object):
 		self.ID = ID
 		self.preference = preference
 		self.gender = gender
+
+	def preference_func(self, person_id):
+		return self.preference[person_id - 1]
 		
 class Table(object):
 	"""docstring for Table"""
@@ -59,8 +64,39 @@ class Table(object):
 	def generate_state():
 		pass
 
-	def calculate_score():
-		pass	
+	def calculate_score(self):
+		# What we gonna do in this method is to traverse half of 
+		# the length of the list since the list represents for 
+		# a table of 2 sides
+		score = 0
+		for i in range(0, int(len(self.guests)/2) - 1):
+			# calculate the top row first
+			# adjacent different in gender add 1 point
+			p1 = self.guests[i]
+			p2 = self.guests[i + 1]
+			if p1.gender != p2.gender:
+				score += 1
+			# h(p1,p2) + h(p2,p1) for adjacent pair of 2 people
+			score += p1.preference_func(p2.ID) + p2.preference_func(p2.ID)
+			# 2 points for opposite pair in different gender
+			p2 = self.guests[int(len(self.guests)/2) + i]
+			if p1.gender != p2.gender:
+				score += 2
+			# h(p1,p2) + h(p2,p1) for opposite pair of 2 people	
+			score += p1.preference_func(p2.ID) + p2.preference_func(p2.ID)
+			# now for the bottom row
+			p1 = self.guests[int(len(self.guests)/2) + i]
+			p2 = self.guests[int(len(self.guests)/2) + i + 1]
+			if p1.gender != p2.gender:
+				score += 1
+			score += p1.preference_func(p2.ID) + p2.preference_func(p2.ID)	
+		#last pair in the table
+		p1 = self.guests[int(len(self.guests)/2) - 1]
+		p2 = self.guests[len(self.guests) - 1]
+		if p1.gender != p2.gender:
+			score += 1
+		score += p1.preference_func(p2.ID) + p2.preference_func(p2.ID)
+		return score
 
 if __name__ == '__main__':
 	main()
