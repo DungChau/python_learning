fd4c656aa5dd10c75b3b51e2a0953016fe277b8d
Dung Chau <viet.lunar@gmail.com>
2013-02-02 01:00:04 -0800

    create new DinnerParty class to encapsulate parse function

--- dinner_party.py
+++ dinner_party.py
@@ -1,21 +1,30 @@
 #!/usr/local/bin/python3
 
-file = open("hw1-inst1.txt")
-# numberOfPeople = int(file.readline())
-matrix = []
-people = []
-
 def main():
-	index = 0
-	for line in file:
-		index += 1
-		lyne = line.split()
-		lyne = [int(i) for i in lyne]
-		# matrix.append(lyne)
-		person = Person(index, lyne)
-		people.append(person)
-		print(people[index - 1].preference)
-	file.close()
+	dinner = DinnerParty()
+	people = dinner.parse_file("hw1-inst1.txt")
+	for person in people:
+		print(person.preference)
+
+class DinnerParty(object):
+	"""docstring for DinnerParty"""
+	def __init__(self):
+		super(DinnerParty, self).__init__()
+	# one interresting fact for me is that even though
+	# a method only take one param python passes 2 params 
+	# to the function: self AND filename
+	def parse_file(self, filename):
+		file = open(filename)
+		index = 0
+		people = []
+		for line in file:
+			index += 1
+			lyne = line.split()
+			lyne = [int(i) for i in lyne]
+			person = Person(index, lyne)
+			people.append(person)
+		file.close()
+		return people
 
 class Person(object):
 	"""docstring for Person"""
@@ -26,10 +35,18 @@ class Person(object):
 		
 class Table(object):
 	"""docstring for Table"""
-	def __init__(self, numberOfChairs):
+	def __init__(self, numberOfChairs, guests):
 		super(Table, self).__init__()
 		self.numberOfChairs = numberOfChairs
-		
+		self.guests = guests
+	def seat():
+		pass
+
+	def generate_state():
+		pass
+
+	def calculate_score():
+		pass	
 
 if __name__ == '__main__':
 	main()
