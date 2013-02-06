6265152ab287ddf38efdb792a222f066d7a56ad2
Dung Chau <viet.lunar@gmail.com>
2013-02-05 18:22:48 -0800

    stop using popup but updating label instead

--- SimCalc.py
+++ SimCalc.py
@@ -7,8 +7,11 @@ class gui(Frame):
 	def __init__(self, master=None):
 		Frame.__init__(self,master)
 		self.master = master
+		self.astring= StringVar()
 		self.label = Label(self,text="Enter 2 numbers:")
 		self.label.pack(side=TOP)
+		self.label2 = Label(self, textvariable = self.astring)
+		self.label2.pack(side=TOP)
 		self.entry1 = Entry(self)
 		self.entry1.pack(side=TOP)
 		self.entry2 = Entry(self)
@@ -24,12 +27,12 @@ class gui(Frame):
 	def reply(self, name):
 		showinfo(title="Reply", message="Your name is: {0}".format(name))
 
-	def add(self, x, y):
-		showinfo(title="Result", message="x^y is: {0}".format(pow(x,y)))		 
+	def gui_pow(self, x, y):
+		self.astring.set(pow(x, y))		 
 
 	def click_submit(self,event):
 		# self.reply(self.entry.get())
-		self.add(int(self.entry1.get()), int(self.entry2.get()))
+		self.gui_pow(int(self.entry1.get()), int(self.entry2.get()))
 
 	def click_quit(self,event):
 		self.master.destroy()
