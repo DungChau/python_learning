7f95a3799bf03b307fb78b78948c119155e0ff8a
Dung Chau <viet.lunar@gmail.com>
2013-02-06 14:59:45 -0800

    check table exists and add write_data() to db

--- db_sqlite.py
+++ db_sqlite.py
@@ -6,6 +6,8 @@ def main():
 	db = Database()
 	db.add_record(("david", "dev", 3400))
 	db.add_record(("ray", "designer", 2000))
+	db.version()
+	# db.write_data() 
 
 	for row in db.query():
 		print(row)
@@ -16,8 +18,9 @@ class Database(object):
 		super(Database, self).__init__()
 		self.conn = sqlite3.connect("db1")
 		self.cur = self.conn.cursor()
-		self.table = self.cur.execute("create table people (name char(30)," 
-									"job char(10), pay int(4))")
+		self.table = self.cur.execute("create table if not exists" 
+											" people (name char(30)," 
+											"job char(10), pay int(4))")
 
 	def add_record(self, record):	
 		self.cur.execute("insert into people values (?,?,?)", record)
@@ -26,6 +29,12 @@ class Database(object):
 	def query(self):
 		self.cur.execute("select * from people")
 		return self.cur.fetchall()
+
+	def version(self):
+		self.cur.execute("select sqlite_version()")
+
+	def write_data(self):	# this write the data to the database
+		self.conn.commit() 	# without it tha data not insertted into db
 
 if __name__ == '__main__':
 	main()
