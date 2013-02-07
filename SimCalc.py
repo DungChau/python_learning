fa38ed64cd5268fc6d1bc5efc927b0a12d448b5b
Dung Chau <viet.lunar@gmail.com>
2013-02-06 20:29:34 -0800

    Setup the layout for simCalc
    It now has multiple buttons and a text entry to get user input and
    display output

--- SimCalc.py
+++ SimCalc.py
@@ -7,28 +7,49 @@ class gui(Frame):
 	def __init__(self, master=None):
 		Frame.__init__(self,master)
 		self.master = master
-		self.astring= StringVar()
-		self.label = Label(self,text="Enter 2 numbers:")
+		self.master.title("A Simple Calculator")
+		self.pack(expand=YES, fill=BOTH)
+
+		self.text= StringVar()
+		self.label = Label(self,text="Enter to the textbox below:")
 		self.label.pack(side=TOP)
-		self.label2 = Label(self, textvariable = self.astring)
-		self.label2.pack(side=TOP)
-		self.entry1 = Entry(self)
-		self.entry1.pack(side=TOP)
-		self.entry2 = Entry(self)
-		self.entry2.pack(side=TOP)
-		self.button = Button(self, text="Submit")
-		self.button.bind("<Button-1>", self.click_submit)
-		self.button.pack(side=LEFT)
-		self.button2 = Button(self, text="Quit")
-		self.button2.bind("<Button-1>", self.click_quit)
-		self.button2.pack(side=RIGHT)
-		self.pack()
+		self.entry = Entry(self, textvariable=self.text)
+		self.entry.pack(side=TOP)
+
+		rows = ["abcd", "0123", "4567", "89()"]
+		for row in rows:
+			frm = Frame(self)
+			frm.pack(side=TOP)
+			for char in row:
+				buttone = Button(frm, text=char, 
+					command=lambda char=char: self.text.set(self.text.get() + char))
+				buttone.pack(side=LEFT)
+
+		self.frm = Frame(self)
+		self.frm.pack(side=TOP)
+		for char in "+-*/=":
+			buttone = Button(self.frm, text=char, 
+				command=lambda char=char: 
+					self.text.set(self.text.get() + " " + char + " "))
+			buttone.pack(side=LEFT)
+
+		self.frm = Frame(self)
+		self.frm.pack(side=BOTTOM)
+		buttone = Button(self.frm, text="result", command= 
+			lambda: self.result(self.text))
+		buttone.pack(side=LEFT)
+		buttone = Button(self.frm, text="clear", command=
+			lambda: self.text.set(" "))
+		buttone.pack(side=RIGHT)
+
+	def result(self, text):
+		pass
 
 	def reply(self, name):
 		showinfo(title="Reply", message="Your name is: {0}".format(name))
 
 	def gui_pow(self, x, y):
-		self.astring.set(pow(x, y))		 
+		self.text.set(pow(x, y))		 
 
 	def click_submit(self,event):
 		# self.reply(self.entry.get())
