bdf83cc9ef38babbbcc678d89c1a90c7de72d5d3
Dung Chau <viet.lunar@gmail.com>
2013-02-07 01:15:23 -0800

    SimCalc working now using eval().
    Error self.text-->need to use self.text.get() to get the value not to
    use str(self.txt)

--- SimCalc.py
+++ SimCalc.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.messagebox import showinfo
 import math
+import parser
 
 class gui(Frame):
 	"""docstring for gui"""
@@ -36,40 +37,48 @@ class gui(Frame):
 		self.frm = Frame(self)
 		self.frm.pack(side=BOTTOM)
 		buttone = Button(self.frm, text="result", command= 
-			lambda: self.result(self.text))
+			lambda: self.text.set(self.result(self.text.get())))
 		buttone.pack(side=LEFT)
 		buttone = Button(self.frm, text="clear", command=
 			lambda: self.text.set(" "))
 		buttone.pack(side=RIGHT)
 
+	def load_exp(self, text):
+		st = parser.expr(text)
+		return st, st.compile() 
+
 	def result(self, text):
-		pass
-
-	def reply(self, name):
-		showinfo(title="Reply", message="Your name is: {0}".format(name))
-
-	def gui_pow(self, x, y):
-		self.text.set(pow(x, y))		 
-
-	def click_submit(self,event):
-		# self.reply(self.entry.get())
-		self.gui_pow(int(self.entry1.get()), int(self.entry2.get()))
+		source_string = str(text)
+		t , s = self.load_exp(source_string)
+		return eval(s)
 
 	def click_quit(self,event):
 		self.master.destroy()
 
+###################################################################
+# this is for future implementation
+###################################################################
 # this gets text from entry box and eval it to a result
 class Interpreter(object):
 	"""docstring for Interpreter"""
-	def __init__(self, arg):
+	def __init__(self, text):
 		super(Interpreter, self).__init__()
-		self.arg = arg
+		self.stack = []
+		self.text = text
+		self.pos = 0
+		self.operator = ["+", "-"]
+
+	def parse(self):
+		pass
+
+	def evaluate(self):
+		pass
 
 # This class represents a literal token aka. a number
-class LiteralToken(object):
-	"""docstring for LiteralToken"""
+class Integer(object):
+	"""docstring for Integer"""
 	def __init__(self, value):
-		super(LiteralToken, self).__init__()
+		super(Integer, self).__init__()
 		self.value = value
 	def toInt(self):
 		return int(self.value)
@@ -80,11 +89,18 @@ class Exp(object):
 		super(Exp, self).__init__()
 		self.arg = arg
 
-class AddExp(Exp):
-	"""docstring for AddExp"""
+class Term(Exp):
+	"""docstring for Term"""
 	def __init__(self, arg):
-		super(AddExp, self).__init__()
+		super(Term, self).__init__()
 		self.arg = arg
+
+class Factor(object):
+	"""docstring for Factor"""
+	def __init__(self, arg):
+		super(Factor, self).__init__()
+		self.arg = arg
+		
 		
 if __name__ == '__main__':
 	win = Tk()
