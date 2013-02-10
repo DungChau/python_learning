103b567106dddf23856bcba51dc534bf29b9bdbe
Dung Chau <viet.lunar@gmail.com>
2013-02-09 20:34:54 -0800

    test indentation

---
+++ letter.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+def letter_freq(line):
+	freq = defaultdict(int)
+	for letter in line:
+		freq[letter] += 1
+	return freq
+def main():
+	print()
+
+if __name__ = '__main__'
+	main()
