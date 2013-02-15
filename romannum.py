e6f5f36a8ff08b48386ccfc21f6f4d0d38de42a0
Dung Chau <viet.lunar@gmail.com>
2013-02-14 23:40:36 -0800

    not working yet

--- romannum.py
+++ romannum.py
@@ -4,24 +4,33 @@ roman_map = zip(("M", "D", "C", "L", "X", "V", "I")
 	,(1000, 500, 100, 50, 10, 5, 1))
 
 def int_to_roman(i):
-	pass
+	result = []
+	rem = 1
+	while rem > 0:
+		rem = int(i/1)
+		rem = rem - 1
+		result.append("I")
+	return "".join(result)
 
 class Test_roman(unittest.TestCase):
 	"""docstring for Test_roman"""
 	def test_one(self):
-		self assertTrue(int_to_roman(1) == "I")
+		self.assertTrue(int_to_roman(1) == "I")
 	def test_three(self):
-		self assertTrue(int_to_roman(3) == "III")
+		self.assertTrue(int_to_roman(3) == "III")
 	def test_four(self):
-		self assertTrue(int_to_roman(4) == "IV")
+		self.assertTrue(int_to_roman(4) == "IV")
 	def test_five(self):
-		self assertTrue(int_to_roman(5) == "V")
+		self.assertTrue(int_to_roman(5) == "V")
 	def test_six(self):
-		self assertTrue(int_to_roman(6) == "VI")
+		self.assertTrue(int_to_roman(6) == "VI")
 	def test_eight(self):
-		self assertTrue(int_to_roman(8) == "VIII")
+		self.assertTrue(int_to_roman(8) == "VIII")
 	def test_nine(self):
-		self assertTrue(int_to_roman(9) == "IX")
+		self.assertTrue(int_to_roman(9) == "IX")
 	def test_thirteen(self):
-		self assertTrue(int_to_roman(13) == "XIII")
+		self.assertTrue(int_to_roman(13) == "XIII")
+
+if __name__ == '__main__':
+	unittest.main()
 		
