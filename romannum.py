e49f9e2129cc4859906a55b9c22f8c3250b38d5a
Dung Chau <viet.lunar@gmail.com>
2013-02-14 23:15:39 -0800

    add test cases for roman numneral

--- romannum.py
+++ romannum.py
@@ -1,6 +1,27 @@
+import unittest
 
 roman_map = zip(("M", "D", "C", "L", "X", "V", "I")
-				,(1000, 500, 100, 50, 10, 5, 1))
+	,(1000, 500, 100, 50, 10, 5, 1))
 
 def int_to_roman(i):
 	pass
+
+class Test_roman(unittest.TestCase):
+	"""docstring for Test_roman"""
+	def test_one(self):
+		self assertTrue(int_to_roman(1) == "I")
+	def test_three(self):
+		self assertTrue(int_to_roman(3) == "III")
+	def test_four(self):
+		self assertTrue(int_to_roman(4) == "IV")
+	def test_five(self):
+		self assertTrue(int_to_roman(5) == "V")
+	def test_six(self):
+		self assertTrue(int_to_roman(6) == "VI")
+	def test_eight(self):
+		self assertTrue(int_to_roman(8) == "VIII")
+	def test_nine(self):
+		self assertTrue(int_to_roman(9) == "IX")
+	def test_thirteen(self):
+		self assertTrue(int_to_roman(13) == "XIII")
+		
